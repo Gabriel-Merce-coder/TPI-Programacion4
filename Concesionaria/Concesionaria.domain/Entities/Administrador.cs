9d8942877de9ca325ae81c3892eec54f81be2ca2
Gabriel-Merce-coder <gabrielmerce910@gmail.com>
2026-08-18 20:05:53 -0300

    Add domain entities and enums

---
+++ Concesionaria/Concesionaria.domain/Entities/Administrador.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concesionaria.domain.Entities
+{
+    internal class Administrador
+    {
+    }
+}
---
+++ Concesionaria/Concesionaria.domain/Enums/EstadoReservaEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concesionaria.domain.Enums
+{
+    internal class EstadoReservaEnum
+    {
+    }
+}
